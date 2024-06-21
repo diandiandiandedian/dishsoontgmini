089d6a7efadd622b52d6ce8976a0c4066aa730ee
zerooooooo1 <xushiduo1@gmail.com>
2024-06-21 15:14:41 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {TonConnectButton, TonConnectUIProvider} from '@tonconnect/ui-react';
+import {Address} from "./compant/Address";
+import {Wallet} from "./compant/Wallet";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // const userFriendlyAddress = useTonAddress();
+    // const rawAddress = useTonAddress(false);
+    return (
+        <TonConnectUIProvider manifestUrl="http://192.168.1.4:3000/tonconnect-manifest.json">
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <TonConnectButton/>
+
+                    <Address></Address>
+                    <Wallet></Wallet>
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </TonConnectUIProvider>
+
+    );
 }
 
 export default App;
