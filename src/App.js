4a31be704e27e17d5f5b35bfe9d4f17791d28f1b
zerooooooo1 <xushiduo1@gmail.com>
2024-06-21 15:23:05 +0800

    修改配置文件

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     // const userFriendlyAddress = useTonAddress();
     // const rawAddress = useTonAddress(false);
     return (
-        <TonConnectUIProvider manifestUrl="http://192.168.1.4:3000/tonconnect-manifest.json">
+        <TonConnectUIProvider manifestUrl="http://https://dishsoontgmini.vercel.app/tonconnect-manifest.json">
             <div className="App">
                 <header className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
